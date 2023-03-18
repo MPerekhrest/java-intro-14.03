1b6819d3c63160d3ad828a61be352eb59458e25b
MPerekhest <perekhrest3@gmail.com>
2023-03-19 00:28:24 +0100

    +

---
+++ src/com/hillel/classwork/lesson2/Main.java
@@ -0,0 +1,19 @@
+package com.hillel.classwork.lesson2;
+
+public class Main {
+
+    /*
+    psvm - create main method
+    sout - print to console
+    Ctrl + Alt + L - format the code
+    */
+    //psvm
+    public static void main(String[] args) {
+
+        //sout
+        System.out.print("Hello in Idea");
+        System.out.print(" the same line");
+        System.out.println();
+        System.out.println("row2");
+    }
+}
