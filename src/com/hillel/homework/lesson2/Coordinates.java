81929011aa27902a5c20a525897eb42d636dd88a
MPerekhest <perekhrest3@gmail.com>
2023-03-23 21:40:07 +0100

    ++

---
+++ src/com/hillel/homework/lesson2/Coordinates.java
@@ -0,0 +1,12 @@
+package com.hillel.homework.lesson2;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        System.out.println("Coordinates:");
+        float lat = 52.1785561F;
+        float lon = 20.9627459F;
+        System.out.println("LAT = " + lat);
+        System.out.println("LON = " + lon);
+    }
+}
+
---
+++ src/com/hillel/homework/lesson2/Lesson2.java
@@ -0,0 +1,21 @@
+package com.hillel.homework.lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        System.out.println("Lesson2:");
+
+        byte a = 10;
+        System.out.println( a);
+        byte b = 20;
+        System.out.println(b);
+        byte c = a;
+        System.out.println(c);
+        a = b;
+        System.out.println(a);
+        b = c;
+        System.out.println(b);
+
+    }
+
+}
