046f6a79467fa98e5438fbb658bbb46730eba23a
MPerekhest <perekhrest3@gmail.com>
2023-04-06 11:54:00 +0200

    Add method homework task Animals

---
+++ src/com/hillel/homework/lesson4/Animals.java
@@ -0,0 +1,19 @@
+package com.hillel.homework.lesson4;
+
+public class Animals {
+    public static void main(String[] args) {
+
+        int cat = calculateEat(8, 3, 50, 30);
+        System.out.println("how much food is needed for cats in a month in grams: " + cat);
+        int dog = calculateEat(24, 3, 50, 30);
+        System.out.println("how much food is needed for dogs in a month in grams: " + dog);
+        System.out.println("How much money do you need per month to run a shelter in dollars: " + (( cat + dog) *2));
+
+    }
+
+    public static int calculateEat(int animals, int eat, int weight, int time) {
+        return (animals * eat * weight * time);
+
+    }
+
+}
