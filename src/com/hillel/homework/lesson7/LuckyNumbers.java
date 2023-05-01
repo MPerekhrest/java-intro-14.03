ea483866ecaa61ae42b1a51ba2233e0a09d0d7eb
MPerekhest <perekhrest3@gmail.com>
2023-05-01 23:12:13 +0200

    Homework lesson 7 Lucky Numbers

--- src/com/hillel/homework/lesson7/LuckyNumbers.java
+++ src/com/hillel/homework/lesson7/LuckyNumbers.java
@@ -3,7 +3,7 @@ package com.hillel.homework.lesson7;
 public class LuckyNumbers {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            if (i / 10 != 4 && i % 10 != 4 && i % 10 != 9) {
+            if (i / 10 != 4 && i % 10 != 4 && i % 10 != 9 && i / 10 != 9) {
                 System.out.println("i = " + i);
             }
         }
