df9ef70d069a5ac4917139475d3445ed702df117
MPerekhest <perekhrest3@gmail.com>
2023-05-01 11:54:23 +0200

    Homework lesson 7 GuessDoWhile

---
+++ src/com/hillel/homework/lesson7/LuckyNumbers.java
@@ -0,0 +1,12 @@
+package com.hillel.homework.lesson7;
+
+public class LuckyNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i / 10 != 4 && i % 10 != 4 && i % 10 != 9) {
+                System.out.println("i = " + i);
+            }
+
+        }
+    }
+}
