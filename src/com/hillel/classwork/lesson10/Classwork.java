be62dfda5febf59111c1bcf3ed373e1184c688f2
MPerekhest <perekhrest3@gmail.com>
2023-07-13 12:15:18 +0200

    Classwork Lesson 10 arrays 2D, 3D, Binary  and factorial

---
+++ src/com/hillel/classwork/lesson10/Classwork.java
@@ -0,0 +1,24 @@
+package com.hillel.classwork.lesson10;
+
+public class Classwork {
+    public static void main(String[] args) {
+        int[] square = new int[9];
+        for (int i = 0; i < square.length; i++) {
+            for (int j = 0; j < square.length; j++) {
+                System.out.print(" * ");
+
+            }
+            System.out.println();
+
+        }
+        int[] rightTriangle = new int[9];
+        for (int i = 0; i < rightTriangle.length + 1; i++) {
+            for (int j=0; j < i; j ++) {
+                System.out.print(" # ");
+
+            }
+            System.out.println();
+        }
+    }
+
+}
